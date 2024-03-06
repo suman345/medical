21237518ea056f83a1dc5eb65dcee9bf485781c7
suman345 <sumandhara345@gmail.com>
2024-03-06 11:31:06 -0500

    upated

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,10 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard';
+    public const ADMIN_DASHBOARD = '/admin/dashboard';
+
+    
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
